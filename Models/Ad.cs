a8eabb4a01d355e5f87ef31a67adadc6bbfe1b71
qqawer <1678627439@qq.com>
2025-12-20 14:57:51 +0800

    feat(Ads):add AdsModel entity

--- Models/Ad.cs
+++ Models/Ad.cs
@@ -1,6 +1,28 @@
-﻿namespace AuthenticationforTheMemeoryGame.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AuthenticationforTheMemeoryGame.Models
 {
+    [Tags("Ads")]
     public class Ad
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        [Column(TypeName = "varchar(256)")]
+        public string AdImageUrl { get; set; } = string.Empty;
+        
+        [Required]
+        [MaxLength(100)]
+        [Column(TypeName = "varchar(100)")]
+        public string ? AdTitle { get; set; }
+
+        [Required]
+        [Column(TypeName = "bit")]
+        public bool IsActive { get; set; }
     }
 }
