903a3c89a79699d1ed1789f8af2e626bed3baea8
qqawer <1678627439@qq.com>
2025-12-20 14:50:48 +0800

    feat(score):add ScoreModel entity

--- Models/Score.cs
+++ Models/Score.cs
@@ -1,6 +1,27 @@
-﻿namespace AuthenticationforTheMemeoryGame.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationforTheMemeoryGame.Models
 {
+    [Tags("Scores")]
     public class Score
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [Required]
+        [Range(1,int.MaxValue)]
+        public int CompletionTimeSeconds { get; set; }
+
+        [Required]
+        [Column(TypeName = "datetime")]
+        public DateTime CompleteAt { get; set; }=DateTime.Now;
+
+        public User User { get; set; } = null;
     }
 }
