d2c64ccbfa9b408260e256eacba20e5575230c07
qqawer <1678627439@qq.com>
2025-12-20 14:36:30 +0800

    feat(user) add UserModel entity

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,28 @@
-﻿namespace AuthenticationforTheMemeoryGame.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace AuthenticationforTheMemeoryGame.Models
 {
+    [Tags("Users")]
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Column(TypeName = "nvarchar(50)")]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        [Column(TypeName = "nvarchar(256)")]
+        public string Password { get; set; }
+
+        [Required]
+        [Column(TypeName = "bit")]
+        public bool IsPaid { get; set; }
+
+        public ICollection<Score> Scores { get; set; }=new List<Score>();
     }
 }
