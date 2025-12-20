9435e63c8b9c497607225285a72181dc74eb8334
qqawer <1678627439@qq.com>
2025-12-20 23:17:55 +0800

    feat:add LoginRequestDto

--- DTOs/Auth/LoginRequestDto.cs
+++ DTOs/Auth/LoginRequestDto.cs
@@ -1,6 +1,15 @@
-﻿namespace AuthenticationforTheMemeoryGame.DTOs.Ads
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationforTheMemeoryGame.DTOs.Ads
 {
     public class LoginRequest
     {
+        [Required(ErrorMessage ="Username is required")]
+        [MaxLength(50)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="Password is required")]
+        [MinLength(6,ErrorMessage ="Password must be at least 6 characters")]
+        public string Password { get; set; } = string.Empty;
     }
 }
