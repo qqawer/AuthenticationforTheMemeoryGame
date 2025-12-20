d8c1f3ddf175e3649efa7fd4452eb2a4496bb0fe
qqawer <1678627439@qq.com>
2025-12-20 23:20:45 +0800

    feat:add LoginResponseDto

---
+++ DTOs/Auth/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthenticationforTheMemeoryGame.DTOs.Auth
+{
+    public class LoginResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public bool IsPaid { get; set; }
+    }
+}
