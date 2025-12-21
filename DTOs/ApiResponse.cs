edcd8bb7c17856ac4a451991b2a204a19a9b983b
qqawer <1678627439@qq.com>
2025-12-21 23:00:12 +0800

    feat: add ApiResponse

---
+++ DTOs/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthenticationforTheMemeoryGame.DTOs
+{
+     public class ApiResponse<T>
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }= string.Empty;
+   
+        public T Data { get; set; }
+    }
+}
