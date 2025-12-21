49e37908a973b79e1e076810a5aac88f7c7d5279
qqawer <1678627439@qq.com>
2025-12-21 22:02:49 +0800

    feat:implement pagination

---
+++ DTOs/Shared/PageResult.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthenticationforTheMemeoryGame.DTOs.Shared
+{
+  
+    public class PagedResult<T>
+    {
+        public List<T> Items { get; set; } = new List<T>(); 
+        public int TotalCount { get; set; }      
+        public int PageNumber { get; set; }      
+        public int PageSize { get; set; }        
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize); 
+    }
+}
--- Services/ScoreService.cs
+++ Services/ScoreService.cs
@@ -1,5 +1,6 @@
 ﻿using AuthenticationforTheMemeoryGame.Data;
 using AuthenticationforTheMemeoryGame.DTOs.Scores;
+using AuthenticationforTheMemeoryGame.DTOs.Shared;
 using AuthenticationforTheMemeoryGame.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,6 +26,38 @@ namespace AuthenticationforTheMemeoryGame.Services
             return result > 0;
         }
 
+        public async Task<PagedResult<LeaderboardResponseDto>> GetLeaderboardAsync(int page, int size)
+        {
+            
+            var query = _dbcontext.Scores.AsQueryable();
+
+            var totalCount = await query.CountAsync();
+
+            var skipAmount = (page - 1) * size;
+
+            var items = await query
+                .Include(s => s.User)
+                .OrderBy(s => s.CompletionTimeSeconds) 
+                .ThenBy(s => s.CompleteAt)             
+                .Skip(skipAmount)                      
+                .Take(size)                           
+                .Select(s => new LeaderboardResponseDto
+                {
+                    Username = s.User.Username,
+                    CompleteTimeSeconds = s.CompletionTimeSeconds,
+                    CompleteAt = s.CompleteAt
+                })
+                .ToListAsync();
+
+  
+            return new PagedResult<LeaderboardResponseDto>
+            {
+                Items = items,
+                TotalCount = totalCount,
+                PageNumber = page,
+                PageSize = size
+            };
+        }
         public async Task<List<LeaderboardResponseDto>> GetLeaderboardAsync(int topN)
         {
             var leaderboard = await _dbcontext.Scores
